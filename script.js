b07da232a3352850424825902ef863e07bc2379c
n4t0b0t <natalieong1990@users.noreply.github.com>
2019-05-27 10:18:10 +0800

    Completed assignment for submission

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const tasks = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
+
+const listContainer = document.querySelector("#todo-list");
+
+function generateList(array) {
+  for (element of array) {
+    const li = document.createElement("li");
+    li.innerHTML =
+      "<p>" + element + "</p> <input id='completed' type='checkbox' />";
+    listContainer.appendChild(li);
+  }
+}
+
+generateList(tasks);
+
+const submitButton = document.querySelector("button");
+
+function addNewItem() {
+  const newItem = document.querySelector("#todo-item").value;
+  tasks.push(newItem);
+  const li = document.createElement("li");
+  li.innerHTML =
+    "<p>" + newItem + "</p> <input id='completed' type='checkbox' />";
+  document.querySelector("#todo-list").appendChild(li);
+}
+
+submitButton.addEventListener("click", addNewItem);
+document.body.addEventListener("keydown", function(e) {
+  if (e.key === 13) {
+    addNewItem();
+  }
+});
+
+listContainer.addEventListener("click", function(event) {
+  if (event.target.type === "checkbox") {
+    event.target.previousElementSibling.classList.toggle("done");
+  }
+});
